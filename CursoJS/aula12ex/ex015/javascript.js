11af9fc3b0bcbb004babc2286ff23eb799dd4aa4
Robert Ferreira <robertferreira2001@hotmail.com>
2023-01-15 21:45:59 -0300

    update

--- CursoJS/aula12ex/ex015/javascript.js
+++ CursoJS/aula12ex/ex015/javascript.js
@@ -1,19 +1,20 @@
-function carregar(){
+function confirmar(){
     var body = window.document.querySelector("body");
-    var div_img = window.document.getElementById("img");
     var img_imagem = window.document.getElementById("imagem");
-    var hora = new Date().getHours();
-    div_img.innerHTML = `Agora são ${hora} horas`;
 
-    if (hora >= 0 && hora < 12) {
+    var manha_checked = window.document.querySelector("input#manha");
+    var tarde_checked = window.document.querySelector("input#tarde");
+    var noite_checked = window.document.querySelector("input#noite");
+
+    if (manha_checked.checked) {
     img_imagem.src = "manha.jpg";
     body.style.backgroundColor = "#ffecbd";
     }
-    else if (hora >= 12 && hora < 18) {
+    else if (tarde_checked.checked) {
     img_imagem.src = "tarde.jpg";
     body.style.backgroundColor = "#a7ccff";
     }
-    else {
+    else if (noite_checked.checked) {
     img_imagem.src = "noite.jpg";
     body.style.backgroundColor = "#44527c";
     }
