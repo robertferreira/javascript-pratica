c4eb04f57ae8ccf91b9516e2a28776c9780a6bbb
Robert Ferreira <robertferreira2001@hotmail.com>
2023-01-17 21:38:26 -0300

    update

--- CursoJS/aula15/ambiente.js
+++ CursoJS/aula15/ambiente.js
@@ -1,3 +1,11 @@
-var num = [5,8,2,9,3];
+var valores = [8, 1, 7, 4, 2, 9];
 
-console.log(num);
+for (var i = 0; i < valores.length; i++){
+console.log(`A posição ${i} tem o valor ${valores[i]}`);
+}
+
+console.log('');
+
+for (let i in valores){
+console.log(`A posição ${i} tem o valor ${valores[i]}`);
+}
