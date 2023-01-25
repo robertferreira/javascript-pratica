e53ca8076c38bd61b11f77edd961e2c5b5deec8d
Robert Ferreira <robertferreira2001@hotmail.com>
2023-01-24 21:25:47 -0300

    update

--- Hcode/aula05/index.js
+++ Hcode/aula05/index.js
@@ -1,3 +1,7 @@
 let numeros = [10, 9, 8 , 7, 6, 5 , 4, 3, 2, 1];
 
-numeros.forEach(valor);
+numeros.forEach(passarArray);
+
+function passarArray (valor, indice){
+    console.log(`O valor é ${valor} e o indice é ${indice}`);
+}
