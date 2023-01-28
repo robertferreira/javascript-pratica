5d9667876af207dbab1929af2114a3bae6d5798f
Robert Ferreira <robertferreira2001@hotmail.com>
2023-01-28 17:51:42 -0300

    add

---
+++ Hcode/curso-javascript-projeto-calculadora-clone/scripts/controller/CalcController.js
@@ -0,0 +1,73 @@
+class CalcController {
+
+    constructor(){
+
+        this.displayCalEl = document.getElementById("display");
+        this.dateEl = document.getElementById("data");
+        this.timeEl = document.getElementById("hora");
+
+        this._currentDate;
+        this._currentTime;
+        this.initialize();
+
+    }
+
+
+    initialize(){
+
+        this.setDisplayDateTime();
+        setInterval( () => {
+            this.setDisplayDateTime();
+        } , 1000);
+    }
+
+
+    initButtonsEvents(){
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+    }
+
+    setDisplayDateTime() {
+        this.displayDate = this.currentDate;
+        this.displayTime = this.currentTime;
+    }
+
+    get displayTime(){
+        return this.timeEl.innerHTML;
+    }
+
+    set displayTime(value){
+        this.timeEl.innerHTML = value;
+    }
+
+    get displayDate(){
+        return this.dateEl.innerHTML;
+    }
+
+    set displayDate(value){
+        this.dateEl.innerHTML = value;
+    }
+
+    get displayCalc(){
+        return this._displayCalcEl.innerHTML;
+    }
+
+    set displayCalc(value){
+        this._displayCalc.innerHTML = value;
+    }
+
+    get currentDate(){
+        return new Date().toLocaleDateString('pt-BR');
+    }
+
+    set currentDate(value){
+        this.currentDate = value;
+    }
+
+    get currentTime(){
+        return new Date().toLocaleTimeString('pt-BR');
+    }
+
+    set currentTime(value){
+        this.currentTime = value;
+    }
+}
