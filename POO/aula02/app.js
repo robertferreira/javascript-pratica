032a4c8773d159848191cf3a6dab5561e816688d
Robert Ferreira <robertferreira2001@hotmail.com>
2023-02-03 17:35:24 -0300

    update

--- POO/aula02/app.js
+++ POO/aula02/app.js
@@ -1,1 +1,22 @@
-console.log('Funcionou');
+var ingredientes = ['mel', 'água', 'sal', 'mostarda'];
+
+var modificado = [];
+
+for (var i=0; i < ingredientes.length; i++){
+
+    var letraInicial = ingredientes[i].charAt(0).toUpperCase();
+
+    var restoTexto = ingredientes[i].slice(1);
+
+    var resultado = letraInicial + restoTexto;
+
+    modificado[i] = resultado;
+
+    console.log(modificado);
+}
+
+var ordenado = modificado.sort((a,b) => {
+    return a.localeCompare(b);
+});
+
+console.log(ordenado);
