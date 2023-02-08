5dcc7bb3a41fcfca8b431de15ee9486ad2fab25c
Robert Ferreira <robertferreira2001@hotmail.com>
2023-02-08 16:09:52 -0300

    new

--- POO/aula02/app.js
+++ POO/aula02/app.js
@@ -2,3 +2,14 @@ import modificador from './modificador.js';
 import ingredientes from './ingredientes.js';
 
 let ingredientesOrdenados = modificador.ordenar(ingredientes, 'nome');
+
+let containerIngredientes = document.getElementById('container-ingredientes');
+
+
+
+/*
+<div class="ingrediente">
+    <img src="img/agriao.png" alt="Foto do Agrião">
+    <p class="nome-ingrediente"></p>
+</div>
+*/
