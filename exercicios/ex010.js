3ecbd056810cfe0b35430e2a87668f1ddd3fd9a6
Robert Ferreira <robertferreira2001@hotmail.com>
2023-02-11 12:51:44 -0300

    add

---
+++ exercicios/ex010.js
@@ -0,0 +1,4 @@
+let n1 = 19.88;
+let dolar = (n1 / 5.22).toFixed(2);
+
+console.log(`Com R$${n1} você pode comprar US$${dolar}`);
---
+++ exercicios/ex016.js
@@ -0,0 +1,3 @@
+let n1 = 3.14;
+
+console.log(`O valor digitado foi ${n1} e a sua porção inteira é ${Math.trunc(n1)}`);
