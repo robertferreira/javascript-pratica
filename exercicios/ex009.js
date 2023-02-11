3d5ddb7d31549704b8f162add8a5cfb7be0d5af5
Robert Ferreira <robertferreira2001@hotmail.com>
2023-02-11 12:31:58 -0300

    add

---
+++ exercicios/ex009.js
@@ -0,0 +1,14 @@
+let n1 = 5;
+
+console.log('-----------------');
+console.log(`${n1} x ${1} = ${n1*1}`);
+console.log(`${n1} x ${2} = ${n1*2}`);
+console.log(`${n1} x ${3} = ${n1*3}`);
+console.log(`${n1} x ${4} = ${n1*4}`);
+console.log(`${n1} x ${5} = ${n1*5}`);
+console.log(`${n1} x ${6} = ${n1*6}`);
+console.log(`${n1} x ${7} = ${n1*7}`);
+console.log(`${n1} x ${8} = ${n1*8}`);
+console.log(`${n1} x ${9} = ${n1*9}`);
+console.log(`${n1} x ${10} = ${n1*10}`);
+console.log('-----------------');
