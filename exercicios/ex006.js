b0b0b752ab065709b0ac4378987837ce97269f15
Robert Ferreira <robertferreira2001@hotmail.com>
2023-02-11 12:19:03 -0300

    add

---
+++ exercicios/ex006.js
@@ -0,0 +1,5 @@
+let n1 = 85;
+
+console.log(`O dobro de ${n1} vale ${n1*2}`);
+console.log(`O triplo de ${n1} vale ${n1*3}`);
+console.log(`O a raiz quadrada de ${n1} vale ${(n1**(1/2)).toFixed(2)}`);
