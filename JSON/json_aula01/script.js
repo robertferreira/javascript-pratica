601db7d34c766daee1d588ffccce100e371b668c
Robert Ferreira <robertferreira2001@hotmail.com>
2023-02-15 17:26:28 -0300

    update

---
+++ JSON/json_aula01/script.js
@@ -0,0 +1,24 @@
+const objs = [
+    {
+        "nome": "Robert",
+        "idade": 21,
+        "esta_trabalhando": true,
+        "hobbies": ["Séries","Filmes","Jogos","Estudo","Programação"],
+        "detalhes_profissao": {
+            "profissao": "Técnico de Informática",
+            "empresa": "Caliman's Comércio de Artesanato"
+        }
+    },
+    {
+        "nome": "João",
+        "idade": 25,
+        "esta_trabalhando": false,
+        "hobbies": ["Academia", "Futebol"],
+        "detalhes_profissao": {
+            "profissao": null,
+            "empresa": null
+        }
+    },
+]
+
+console.log(objs);
