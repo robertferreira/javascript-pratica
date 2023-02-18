4db36d1c9f73cdc3d2819fdf275e1b372357d1f3
Robert Ferreira <robertferreira2001@hotmail.com>
2023-02-17 22:32:53 -0300

    update

--- API/aula01/index.js
+++ API/aula01/index.js
@@ -1,8 +1,9 @@
 const express = require ('express');
 const server = express();
+const filmes = require('./src/data/filmes.json')
 
-server.get('/usuario', (req,res) => {
-    return res.json({usuario: 'Robert'});
+server.get('/filmes', (req,res) => {
+    return res.json({filmes});
 });
 
 server.listen(3000, () => {
