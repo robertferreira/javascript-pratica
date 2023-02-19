1bc0c3e1e335e50f1efca0b26a93f9ccc1877584
Robert Ferreira <robertferreira2001@hotmail.com>
2023-02-18 21:29:38 -0300

    update

--- JSON/json_aula02/index.js
+++ JSON/json_aula02/index.js
@@ -2,3 +2,12 @@ var requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/super
 
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
+
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+    var superHeroes = request.response;
+    populateHeader(superHeroes);
+    showHeroes(superHeroes);
+  }
