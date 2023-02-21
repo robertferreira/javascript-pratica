25c18eb2480aaaf5bf1def003dd105c61b9ee53f
Robert Ferreira <robertferreira2001@hotmail.com>
2023-02-21 16:21:35 -0300

    update

--- API/aula02/js/scripts.js
+++ API/aula02/js/scripts.js
@@ -1,1 +1,34 @@
-// afeddb92a9abbf878c890227729fa62c
+// Variáveis e seleção de elementos
+const apiKey = "afeddb92a9abbf878c890227729fa62c";
+const apiCountryURL = "https://countryflagsapi.com/png/";
+
+const cityInput = document.getElementById('city-input');
+const searchBtn = document.getElementById('search');
+
+const cityElement = document.getElementById('city');
+const tempElement = document.querySelector('#temperature span');
+const descElement = document.getElementById('description');
+const weatherIconElement = document.getElementById('weather-icon');
+const countryElement = document.getElementById('country');
+const umidityElement = document.querySelector('#umidity span');
+const windElement = document.querySelector('#wind span');
+
+// Funções
+const getWeatherData = async(city) => {
+
+}
+
+const showWeatherData = (city) => {
+    console.log(city);
+}
+
+// Eventos
+searchBtn.addEventListener("click", (e) => {
+
+    e.preventDefault();
+
+    const city = cityInput.value;
+
+    showWeatherData(city);
+
+})
