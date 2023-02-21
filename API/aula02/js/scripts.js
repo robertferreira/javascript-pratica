bfb3788ed96a6bb216bb3cf243d7265b41a42da7
Robert Ferreira <robertferreira2001@hotmail.com>
2023-02-21 20:38:13 -0300

    update

--- API/aula02/js/scripts.js
+++ API/aula02/js/scripts.js
@@ -29,7 +29,10 @@ const showWeatherData = async (city) => {
     cityElement.innerText = data.name;
     tempElement.innerText = parseInt(data.main.temp);
     descElement.innerText = data.weather[0].description;
-    weatherIconElement.setAttribute("src", ``);
+    weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    countryElement.setAttribute("src", apiCountryURL + data.sys.country);
+    umidityElement.innerText = `${data.main.umidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`
 
 }
 
