7d9f87c107f7c4ca6799941a4c2370b961d3a2ad
Robert Ferreira <robertferreira2001@hotmail.com>
2023-02-21 20:30:25 -0300

    update

--- API/aula02/js/scripts.js
+++ API/aula02/js/scripts.js
@@ -15,13 +15,22 @@ const windElement = document.querySelector('#wind span');
 
 // Funções
 const getWeatherData = async(city) => {
-    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid${apiKey}&lang=pt_br`;
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
 
-   
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json();
+
+    return data;
 }
 
-const showWeatherData = (city) => {
-    console.log(city);
+const showWeatherData = async (city) => {
+    const data = await getWeatherData(city);
+
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute("src", ``);
+
 }
 
 // Eventos
