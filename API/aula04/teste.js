36c30a0b9c02efa3027c96550a05d55155c9887a
Robert Ferreira <robertferreira2001@hotmail.com>
2023-02-22 19:09:39 -0300

    update

--- API/aula04/teste.js
+++ API/aula04/teste.js
@@ -1,10 +1,27 @@
 const estado = 'MG';
 const URL = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estado}/distritos`;
+const divElement = document.getElementById('resultado');
+const inputElement = document.getElementById('input');
+
 
 async function testeApi(){
     const api = await fetch (URL);
     const data = await api.json();
+    var cidadeId = 0;
+    var cidade = inputElement.value.toUpperCase();
+    
+    for (i=0; i < data.length; i++){
 
-    console.log(data);
+    if (data[cidadeId].municipio.nome.toUpperCase().indexOf(cidade) == -1){
+        divElement.innerText = 'Cidade não encontrada';
+        cidadeId++;
+    }
+
+    else {
+        divElement.innerText = cidade;
+    }
+    }
+    
+
 
 }
