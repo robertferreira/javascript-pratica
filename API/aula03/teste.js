3396144ae742a2de8b41e442ce8f02f54d928eae
Robert Ferreira <robertferreira2001@hotmail.com>
2023-02-21 22:30:44 -0300

    update

--- API/aula03/teste.js
+++ API/aula03/teste.js
@@ -2,23 +2,22 @@ const URL = `https://servicodados.ibge.gov.br/api/v1/paises/`;
 const divElement = document.getElementById('resultado');
 const inputElement = document.getElementById('input');
 
-
 async function Api() { 
 
 var paisId = 0;
 
 event.preventDefault();
 
-var pais = inputElement.value;
+var pais = (inputElement.value.toUpperCase());
 
 const conexaoApi = await fetch(URL);
 const dados = await conexaoApi.json();
 
-for (i=0; dados[i].nome.abreviado.indexOf(pais) == -1; i++){
+for (i=0; dados[i].nome.abreviado.toUpperCase().indexOf(pais) == -1; i++){
 paisId ++;
 }
 
-divElement.innerText = dados[paisId].nome.abreviado;
+divElement.innerText = dados[paisId].id["ISO-3166-1-ALPHA-2"] + " - " +dados[paisId].nome.abreviado;
 
 }
 
