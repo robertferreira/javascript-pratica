1eeb39e3009f3973be63dd3ae1f139b5cee1f87c
Robert Ferreira <robertferreira2001@hotmail.com>
2023-02-24 17:20:24 -0300

    update

--- ReactJS/Hora de Codar/meu-projeto/src/App.js
+++ ReactJS/Hora de Codar/meu-projeto/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   return (
-    <div className="">
-    
+    <div className="App">
+      <h1> Olá React! </h1>
+      <p> Meu primeiro App </p>
     </div>
+    
   );
 }
 
