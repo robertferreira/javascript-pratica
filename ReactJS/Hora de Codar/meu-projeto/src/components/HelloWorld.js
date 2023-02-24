6fc61d6a3c772566f476c0fe71351991065f43a5
Robert Ferreira <robertferreira2001@hotmail.com>
2023-02-24 20:54:20 -0300

    update

--- ReactJS/Hora de Codar/meu-projeto/src/components/HelloWorld.js
+++ ReactJS/Hora de Codar/meu-projeto/src/components/HelloWorld.js
@@ -1,7 +1,11 @@
+import Frase from './Frase.js'
+
 function HelloWorld() {
     return (
         <div>
+            <Frase/>
             <h1>Meu primeiro componente</h1>
+            <Frase/>
         </div>
     )
 }
