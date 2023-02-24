1065fc31fdffc1e1579afae22c1a29926022bc56
Robert Ferreira <robertferreira2001@hotmail.com>
2023-02-24 17:40:24 -0300

    update

--- ReactJS/Hora de Codar/meu-projeto/src/App.js
+++ ReactJS/Hora de Codar/meu-projeto/src/App.js
@@ -2,10 +2,20 @@ import './App.css';
 
 function App() {
   
+  const name = 'Robert';
+
+  function sum(a,b){
+    return a + b;
+  }
+
+  const url = "https://via.placeholder.com/200";
+
   return (
     <div className="App">
-      <h1> Olá React! </h1>
-      <p> Meu primeiro App </p>
+      <h2> Alterando o JSX </h2>
+      <p> Olá, {name}</p>
+      <p> Soma: {sum(2,2)}</p>
+      <img src={url} alt="Minha Imagem" />
     </div>
     
   );
