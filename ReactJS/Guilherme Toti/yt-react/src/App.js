7ebb16e06c070ef54b552b243e5fbf7e8a9a8561
Robert Ferreira <robertferreira2001@hotmail.com>
2023-02-24 16:09:16 -0300

    update

--- ReactJS/Guilherme Toti/yt-react/src/App.js
+++ ReactJS/Guilherme Toti/yt-react/src/App.js
@@ -1,10 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+
+class MyButton extends Component {
+  render() {
+    return <button> {this.props.label}</button>
+  }
+}
+
+class MyLabel extends Component {
+  render() {
+    return <p> {this.props.text}</p>
+  }
+}
 
 function App() {
-  return (
-    <p> Hello World!</p>
-  );
+
+  class App extends Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+
+      }
+    }
+
+    render() {
+      return (
+        <div className='App'>
+          <p> Hello, World!</p>
+          <MyLabel text="Escrevendo pelo app" />
+          <MyButton label="Botão 1" />
+          <MyButton label="Botão 2" />
+          <MyButton label="Botão 3" />
+          <MyButton label="Botão 4" />
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
