06af907970dd197a0c69b0019057f9607ca5d7c1
Robert Ferreira <robertferreira2001@hotmail.com>
2023-02-25 20:34:01 -0300

    update

--- ReactJS/Hora de Codar/meu-projeto/src/App.js
+++ ReactJS/Hora de Codar/meu-projeto/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Evento from './components/Evento';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-    <h1>  Testando Eventos </h1>
-    <Evento numero={1}/>
-    <Evento numero={2}/>
+    <Form/>
     </div>
   );
 }
