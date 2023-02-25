faa7f8617bdb16da5ae4ce11d6f275122c5bb68c
Robert Ferreira <robertferreira2001@hotmail.com>
2023-02-24 21:53:22 -0300

    update

--- ReactJS/Hora de Codar/meu-projeto/src/App.js
+++ ReactJS/Hora de Codar/meu-projeto/src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMySname';
 import Pessoa from './components/Pessoa';
+import List from './components/List';
 
 function App() {
   return (
     <div className="App">
       <HelloWorld/>
       <Pessoa nome="Robert" idade="21" profissao="Técnico de Informática" foto="https://via.placeholder.com/150"/>
+      <List/>
     </div>
-    
   );
 }
 
