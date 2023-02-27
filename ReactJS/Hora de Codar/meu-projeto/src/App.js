92006cc24034b59ca48829e24b9bf2a8b0e17a61
Robert Ferreira <robertferreira2001@hotmail.com>
2023-02-27 20:48:06 -0300

    update

--- ReactJS/Hora de Codar/meu-projeto/src/App.js
+++ ReactJS/Hora de Codar/meu-projeto/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import Form from './components/Form';
-import Evento from './components/Evento';
+import Condicional from './components/Condicional';
 
 function App() {
   return (
     <div className="App">
-    <Evento/>
-    <Form/>
+      <h1>Renderização Condicional</h1>
+      <Condicional/>
     </div>
   );
 }
---
+++ ReactJS/Hora de Codar/meu-projeto/src/components/Condicional.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+function Condicional() {
+
+    function enviarEmail(e){
+        e.preventDefault();
+        console.log('Testando');
+    }
+
+    return (
+        <div>
+            <h2>Cadastre o seu e-mail:</h2>
+            <form>
+                <input type="email" placeholder="Digite o seu e-mail..." />
+                <button type="submit" onClick={enviarEmail}> Enviar e-mail</button>
+            </form>
+        </div>
+    );
+}
+
+export default Condicional;
