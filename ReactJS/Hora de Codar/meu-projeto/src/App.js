f01a0330da9cb134b85d1ffcb26b1dba6e552a88
Robert Ferreira <robertferreira2001@hotmail.com>
2023-03-01 17:38:51 -0300

    UPDATE

--- ReactJS/Hora de Codar/meu-projeto/src/App.js
+++ ReactJS/Hora de Codar/meu-projeto/src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
-import OutraLista from './components/OutraLista';
 
 function App() {
 
-  const meusItens = ['React', 'Vue', 'Angular'];
-
   return (
     <div className="App">
-      <h1>Renderização de Listas</h1>
-      <OutraLista itens={meusItens}/>
-      <OutraLista itens={[]}/>
+      <h1>State Lift</h1>
     </div>
   );
 }
