4317640528ce5c9a3387f630ff6e872233c757a6
Robert Ferreira <robertferreira2001@hotmail.com>
2023-03-03 20:02:45 -0300

    new

--- ReactJS/Hora de Codar/meu-projeto/src/App.js
+++ ReactJS/Hora de Codar/meu-projeto/src/App.js
@@ -2,12 +2,12 @@ import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 function App() {
 
-  const [nome, setNome] = useState();
-
   return (
     <Router>
       <ul>
         <li><Link to="/"> Home </Link></li>
+        <li><Link to="/empresa"> Empresa </Link></li>
+        <li><Link to="/contato"> Contato </Link></li>
       </ul>
     </Router>
   );
