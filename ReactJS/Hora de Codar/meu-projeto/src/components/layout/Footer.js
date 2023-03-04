f7a8fae2613dac27ef22fc772664467784058011
Robert Ferreira <robertferreira2001@hotmail.com>
2023-03-04 10:57:51 -0300

    update

--- ReactJS/Hora de Codar/meu-projeto/src/components/layout/Footer.js
+++ ReactJS/Hora de Codar/meu-projeto/src/components/layout/Footer.js
@@ -1,9 +1,10 @@
 import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
+import styles from './Footer.module.css'
 
 function Footer() {
     return(
     <footer>
-        <ul>
+        <ul className={styles.social_list}>
             <li> <FaFacebook/> </li>
             <li> <FaInstagram/> </li>
             <li> <FaLinkedin/> </li>
