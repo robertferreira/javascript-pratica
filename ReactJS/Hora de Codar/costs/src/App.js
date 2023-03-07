03e9ab0bfa774fc6f63ac1374552a35a596ab90f
Robert Ferreira <robertferreira2001@hotmail.com>
2023-03-07 16:03:52 -0300

    NEW

--- ReactJS/Hora de Codar/costs/src/App.js
+++ ReactJS/Hora de Codar/costs/src/App.js
@@ -1,8 +1,27 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
 function App() {
   return (
-    <div className="App">
-      <p> Costs </p>
-    </div>
+    <BrowserRouter> 
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/Company">Empresa</Link>
+        <Link to="/Contact">Contato</Link>
+        <Link to="/NewProject">Novo Projeto</Link>
+      </ul>
+
+        <Container>
+          <Routes>
+            <Route path= "/" element={<Home/>}/> 
+            <Route path= "/Company" element={<Company/>}/> 
+            <Route path= "/Contact" element={<Contact/>}/> 
+            <Route path= "/NewProject" element={<NewProject/>}/>    
+          </Routes>
+        </Container>
+
+      <p>Footer</p>
+
+    </BrowserRouter>
   );
 }
 
