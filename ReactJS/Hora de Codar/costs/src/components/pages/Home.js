3cef43303f42dc8f835fc9c3c2caf18d0c338c60
Robert Ferreira <robertferreira2001@hotmail.com>
2023-03-09 15:47:38 -0300

    update

--- ReactJS/Hora de Codar/costs/src/components/pages/Home.js
+++ ReactJS/Hora de Codar/costs/src/components/pages/Home.js
@@ -1,8 +1,14 @@
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+
 function Home() {
     return (
-        <>
-        <h1> Home </h1>
-        </>
+        <section className={styles.home_container}>
+        <h1> Bem-vindo ao <span>Costs</span></h1>
+        <p> Comece a gerenciar os seus projetos agora mesmo!</p>
+        <a href="/"> Criar Projeto </a>
+        <img src={savings} alt="Costs" />
+        </section>
     )
 }
 
