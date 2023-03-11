d9ef998a7dfd0f1aaed13daed32a0d865a49403e
Robert Ferreira <robertferreira2001@hotmail.com>
2023-03-11 20:59:16 -0300

    update

--- ReactJS/Hora de Codar/costs/src/components/pages/NewProject.js
+++ ReactJS/Hora de Codar/costs/src/components/pages/NewProject.js
@@ -1,11 +1,12 @@
 import styles from './NewProject.module.css'
+import ProjectForm from '../project/ProjectForm';
 
 function NewProject() {
     return (
         <div className={styles.newproject_container}>
             <h1> Criar Projeto </h1>
             <p> Crie seu projeto para depois adicionar os serviços </p>
-            <p> Formulário </p>
+            <ProjectForm/>
         </div>
     )
 }
--- ReactJS/Hora de Codar/costs/src/components/project/ProjectForm.js
+++ ReactJS/Hora de Codar/costs/src/components/project/ProjectForm.js
@@ -1,3 +1,19 @@
 function ProjectForm() {
-    
+    return (
+        <form>
+            <div>
+                <input type="text" placeholder="Insira o nome do projeto" />
+            </div>
+            <div>
+                <input type="number" placeholder="Insira o orçamento total" />
+            </div>
+            <div>
+                <select name="category_id">
+                    <option disabled> Selecione a categoria </option>
+                </select>
+            </div>
+        </form>
+    )
 }
+
+export default ProjectForm;
