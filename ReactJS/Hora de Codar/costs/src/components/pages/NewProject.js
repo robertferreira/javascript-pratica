5384000721b283677d94a5d62355cd55c2423863
Robert Ferreira <robertferreira2001@hotmail.com>
2023-03-11 20:54:45 -0300

    update

--- ReactJS/Hora de Codar/costs/src/components/pages/NewProject.js
+++ ReactJS/Hora de Codar/costs/src/components/pages/NewProject.js
@@ -2,9 +2,10 @@ import styles from './NewProject.module.css'
 
 function NewProject() {
     return (
-        <div>
+        <div className={styles.newproject_container}>
             <h1> Criar Projeto </h1>
             <p> Crie seu projeto para depois adicionar os serviços </p>
+            <p> Formulário </p>
         </div>
     )
 }
