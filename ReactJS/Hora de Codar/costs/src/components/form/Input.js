38c2fa5af009a054bbe9043b1b1cb87e97acb243
Robert Ferreira <robertferreira2001@hotmail.com>
2023-03-12 19:26:00 -0300

    update

--- ReactJS/Hora de Codar/costs/src/components/form/Input.js
+++ ReactJS/Hora de Codar/costs/src/components/form/Input.js
@@ -1,10 +1,17 @@
 import styles from './Input.module.css'
 
-function Input({type, text, name, placeholder, handleOnChange, value}) {
+function Input({ type, text, name, placeholder, handleOnChange, value }) {
     return (
-        <div>
-            <label></label>
-            <input type="text" />
+        <div className={styles.form_control}>
+            <label htmlFor={name}>{text}:</label>
+            <input
+                type={type}
+                name={name}
+                id={name}
+                placeholder={placeholder}
+                onChange={handleOnChange}
+                value={value}
+            />
         </div>
     )
 }
--- ReactJS/Hora de Codar/costs/src/components/project/ProjectForm.js
+++ ReactJS/Hora de Codar/costs/src/components/project/ProjectForm.js
@@ -1,19 +1,26 @@
 import styles from './ProjectForm.module.css'
+import Input from '../form/Input';
+import Select from '../form/Select';
 
 function ProjectForm() {
     return (
         <form className={styles.form}>
-            <div>
-                <input type="text" placeholder="Insira o nome do projeto" />
-            </div>
-            <div>
-                <input type="number" placeholder="Insira o orçamento total" />
-            </div>
-            <div>
-                <select name="category_id">
-                    <option disabled selected> Selecione a categoria </option>
-                </select>
-            </div>
+            <Input
+                type="text"
+                text="Nome do Projeto"
+                name="name"
+                placeholder="Insira o nome do projeto"
+            />
+            <Input
+                type="number"
+                text="Orçamento do Projeto"
+                name="budget"
+                placeholder="Insira o orçamento total"
+            />
+            <Select
+                name="category_id"
+                text="Selecione a categoria"
+            />
             <div>
                 <input type="submit" value="Criar projeto" />
             </div>
