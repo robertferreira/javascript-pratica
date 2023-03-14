60ec3efad02799a5ea2e2766e028d212b2074522
Robert Ferreira <robertferreira2001@hotmail.com>
2023-03-14 15:32:20 -0300

    update

--- ReactJS/Hora de Codar/costs/src/components/project/ProjectForm.js
+++ ReactJS/Hora de Codar/costs/src/components/project/ProjectForm.js
@@ -1,9 +1,26 @@
-import styles from './ProjectForm.module.css'
+import { useState } from 'react';
+
+import styles from './ProjectForm.module.css';
 import Input from '../form/Input';
 import Select from '../form/Select';
 import SubmitButton from '../form/SubmitButton';
 
 function ProjectForm({btnText}) {
+
+    const [categories, setCategories] = useState([]);
+
+    fetch("http://localhost:5000/categories",{
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then((resp) => resp.json())
+    .then((data) => {
+        setCategories(data)
+    })
+    .catch((err) => console.log(err))
+
     return (
         <form className={styles.form}>
             <Input
@@ -21,6 +38,7 @@ function ProjectForm({btnText}) {
             <Select
                 name="category_id"
                 text="Selecione a categoria"
+                options={categories}
             />
             <SubmitButton text={btnText}/>
         </form>
