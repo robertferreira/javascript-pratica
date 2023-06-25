39e08542ceefed3875d797dd736ccd193b852be3
Robert Ferreira <robertferreira2001@hotmail.com>
2023-06-24 22:39:21 -0300

    update

--- NodeJS/person.js
+++ NodeJS/person.js
@@ -1,12 +1,12 @@
 class Person {
-    constructor(name) {
-        this.name = name;
-    }
-    sayMyName(){
-        return `My name is ${this.name}`;
-    }
+  constructor(name) {
+    this.name = name;
+  }
+  sayMyName() {
+    return `My name is ${this.name}`;
+  }
 }
 
 module.export = {
-    Person,
+  Person,
 };
