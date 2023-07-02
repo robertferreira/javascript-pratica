08ecbf6f1ff68c3b66c4fea2e6810d86434927fe
Robert Ferreira <robertferreira2001@hotmail.com>
2023-07-02 17:07:19 -0300

    update

--- NodeJS/Curso de Node.js/Test/index.js
+++ NodeJS/Curso de Node.js/Test/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Seja bem-vindo ao meu app!");
+});
 
-
-app.listen(8081);
+app.listen(8081, () => {
+  console.log("Servidor rodando na url http://localhost:8081");
+});
