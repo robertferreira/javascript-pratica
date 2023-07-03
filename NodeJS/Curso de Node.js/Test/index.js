6abf3f199f6957db497282eff0752be78cee0ff9
Robert Ferreira <robertferreira2001@hotmail.com>
2023-07-02 21:24:25 -0300

    update

--- NodeJS/Curso de Node.js/Test/index.js
+++ NodeJS/Curso de Node.js/Test/index.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
   res.send("Seja bem-vindo ao meu app!");
 });
 
+app.get("/ola", (req, res) => {
+  res.send("Ola");
+});
+
 app.listen(8081, () => {
   console.log("Servidor rodando na url http://localhost:8081");
 });
