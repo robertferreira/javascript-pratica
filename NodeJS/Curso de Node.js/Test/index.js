2d6b0f754c1c66541021d77ee4143f159a9686d9
Robert Ferreira <robertferreira2001@hotmail.com>
2023-07-07 20:50:31 -0300

    update

--- NodeJS/Curso de Node.js/Test/index.js
+++ NodeJS/Curso de Node.js/Test/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars");
+const Sequelize = require("sequelize");
 
 // Config
 // Template Engine
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+// Conexão com o banco de dados MySQL
+const sequelize = new Sequelize("test", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+});
 
 app.listen(8081, () => {
   console.log("Servidor rodando!");
