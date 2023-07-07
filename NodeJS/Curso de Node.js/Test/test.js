2acb875526ad9b52d168382f089426c05933ffd0
Robert Ferreira <robertferreira2001@hotmail.com>
2023-07-06 21:26:02 -0300

    update

--- NodeJS/Curso de Node.js/Test/test.js
+++ NodeJS/Curso de Node.js/Test/test.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("test", "root", "123456", {
+const sequelize = new Sequelize("test", "root", "", {
   host: "localhost",
   dialect: "mysql",
 });
@@ -9,4 +9,25 @@ const Postagem = sequelize.define("postagens", {
   conteudo: { type: Sequelize.TEXT },
 });
 
-Postagem.sync({ force: true });
+//Postagem.sync({ force: true });
+
+/*Postagem.create({
+  titulo: "UM TITULO QUALQUER",
+  conteudo: "CONTEUDO QUALQUER",
+});*/
+
+const Usuario = sequelize.define("usuarios", {
+  nome: { type: Sequelize.STRING },
+  sobrenome: { type: Sequelize.STRING },
+  idade: { type: Sequelize.INTEGER },
+  email: { type: Sequelize.STRING },
+});
+
+//Usuario.sync({force: true});
+
+Usuario.create({
+  nome: "Robert",
+  sobrenome: "Ferreira",
+  idade: 22,
+  email: "robertferreira@gmail.com"
+})
