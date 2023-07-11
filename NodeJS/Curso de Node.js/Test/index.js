d82373cbba27c405c6b4584e864c81c5616ee57f
Robert Ferreira <robertferreira2001@hotmail.com>
2023-07-11 17:43:06 -0300

    update

--- NodeJS/Curso de Node.js/Test/index.js
+++ NodeJS/Curso de Node.js/Test/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
 const Sequelize = require("sequelize");
 
 // Config
 // Template Engine
 app.engine("handlebars", handlebars.engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // Conexão com o banco de dados MySQL
 const sequelize = new Sequelize("test", "root", "", {
   host: "localhost",
@@ -18,7 +22,9 @@ app.get("/cad", (req, res) => {
 });
 
 app.post("/add", (req, res) => {
-  res.send("Formulário Recebido");
+  let titulo = req.body.titulo;
+  let conteudo = req.body.conteudo;
+  res.send(`Texto: ${titulo} Conteúdo: ${conteudo}`);
 });
 
 app.listen(8081, () => {
