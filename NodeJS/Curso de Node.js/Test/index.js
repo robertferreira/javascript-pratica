e19ee4ac278d8f502b1d2928bd9a606239fcddbf
Robert Ferreira <robertferreira2001@hotmail.com>
2023-07-16 15:30:39 -0300

    update

--- NodeJS/Curso de Node.js/Test/index.js
+++ NodeJS/Curso de Node.js/Test/index.js
@@ -46,6 +46,16 @@ app.post("/add", (req, res) => {
     });
 });
 
+app.get("/deletar/:id", (req, res) => {
+  Post.destroy({ where: { id: req.params.id } })
+    .then(() => {
+      res.send("Postagem deletada com sucesso!");
+    })
+    .catch((erro) => {
+      res.send("Esta postagem não existe!");
+    });
+});
+
 app.listen(8081, () => {
   console.log("Servidor rodando!");
 });
