031cfc4696e3e9079c801145046b35e4bde16260
Robert Ferreira <robertferreira2001@hotmail.com>
2023-07-16 19:17:34 -0300

    update

--- NodeJS/Curso de Node.js/trabalhandoComOMongo/aprendendo.js
+++ NodeJS/Curso de Node.js/trabalhandoComOMongo/aprendendo.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/bancoTeste")
+  .connect("mongodb://localhost:27017/bancoTeste", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("MongoDB Conectado");
   })
